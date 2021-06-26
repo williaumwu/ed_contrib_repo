95668d788f2f5def2003111c576f809365d45237
Gary <gary@elasticdev.io>
2021-06-26 22:06:51 +0800

    update

--- stacks/_ed_configs/hello_world_stack/_main/run.py
+++ stacks/_ed_configs/hello_world_stack/_main/run.py
@@ -1,5 +1,7 @@
 def run(stackargs):
 
+    import json
+
     # instantiate stack
     stack = newStack(stackargs)
 
@@ -8,7 +10,7 @@ def run(stackargs):
     stack.parse.add_required(key="hostname")
 
     # Add shelloutconfigs
-    stack.add_shelloutconfig('williaumwu:::demo-repo::hello_world_script')
+    stack.add_shelloutconfig('williaumwu:::demo-repo::hello_world_script',"script")
 
     # Add hostgroups
     stack.add_hostgroups("williaumwu:::demo-repo::hello_world_group", "latest_hostgroup")
@@ -21,11 +23,25 @@ def run(stackargs):
     # add run environmental variables
     _env_vars = {"SHOW_VERSION":stack.show_version}
     _env_vars["ENV"] = "demo"
-
     stack.add_host_env_vars_to_run(_env_vars)
 
     # print out variables on saas dashboard/run
     stack.publish(_env_vars)
+
+    # demo orchestration shellout execution
+    orchestr_env_vars = {"SHOW_VERSION":int(stack.show_version) + 1 }
+    orchestr_env_vars["ENV"] = "demo"
+    inputargs = {"display":True}
+    inputargs["human_description"] = 'Demos Show Version +1 in the orchestration'
+    inputargs["env_vars"] = json.dumps(orchestr_env_vars)
+    stack.script.run(**inputargs)
+
+    orchestr_env_vars = {"SHOW_VERSION":int(stack.show_version) + 2 }
+    orchestr_env_vars["ENV"] = "demo"
+    inputargs = {"display":True}
+    inputargs["human_description"] = 'Demos Show Version +2 in the orchestration'
+    inputargs["env_vars"] = json.dumps(orchestr_env_vars)
+    stack.script.run(**inputargs)
 
     # execute orders on host
     stack.add_groups_to_host(groups=stack.latest_hostgroup,hostname=stack.hostname)
