0f427eb02c74b1c6a918b6cd6d964b666950d36d
Gary <gary@elasticdev.io>
2021-07-05 22:46:43 +0800

    update

--- stacks/_ed_configs/ferrari_platform/_main/run.py
+++ stacks/_ed_configs/ferrari_platform/_main/run.py
@@ -20,19 +20,19 @@ def run(stackargs):
     stack.init_execgroups()
     stack.init_shelloutconfigs()
 
-    stack.set_variable("chrootfiles_dest_dir","/var/tmp/share/{}".format(stack.build_dir))
+    stack.set_variable("group_dest_dir","/var/tmp/share/{}".format(stack.build_dir))
 
-    env_vars = {"CHROORFILES_DEST_DIR":stack.chrootfiles_dest_dir}
-    env_vars["F1_DIR"] = stack.chrootfiles_dest_dir
+    env_vars = {"group_dest_dir":stack.group_dest_dir}
+    env_vars["F1_DIR"] = stack.group_dest_dir
 
     inputargs = {"display":True}
     inputargs["env_vars"] = json.dumps(env_vars)
-    inputargs["human_description"] = "Creating platform with wheels {}".format(stack.wheels.asset)
+    inputargs["human_description"] = "Creating platform with wheels {}".format(stack.wheels.name)
     stack.wheels.insert(**inputargs)
 
     inputargs = {"display":True}
     inputargs["env_vars"] = json.dumps(env_vars)
-    inputargs["human_description"] = "Creating platform with core {}".format(stack.core.asset)
+    inputargs["human_description"] = "Creating platform with core {}".format(stack.core.name)
     stack.core.insert(**inputargs)
 
     return stack.get_results()
